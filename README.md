c579b1d5b62a029846e1036a42e1a3756ed40d2f
Sam <shneydik.sam@gmail.com>
2024-10-19 18:53:21 +0200

    Added settings

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Squash_Center
 A website where you can book squash courts, rent equipment for the game, buy a gym membership and much more.
+
+
+[![Django CI](https://github.com/senya2006/Squash_Center/actions/workflows/django-ci.yml/badge.svg)](https://github.com/senya2006/Squash_Center/actions/workflows/django-ci.yml)
+
+[![Django CI](https://github.com/senya2006/Squash_Center/actions/workflows/django-ci.yml/badge.svg?event=public)](https://github.com/senya2006/Squash_Center/actions/workflows/django-ci.yml)
---
+++ src/config/settings/stg.py
@@ -0,0 +1,23 @@
+from config.settings.base import *  # NOQA
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-280gtlp9=+j3h%!1z0n_pi!f=@!-1(!aix58@gq&c&zh3_m4=0'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # NOQA
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
